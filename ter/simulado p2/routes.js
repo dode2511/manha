277898b53a32b7a6f96e458a10528bdbcd2f924e
dode2511 <81193494+dode2511@users.noreply.github.com>
2023-05-23 09:47:47 -0300

    Update routes.js

--- ter/simulado p2/routes.js
+++ ter/simulado p2/routes.js
@@ -1,11 +1,25 @@
 import { Router } from "express"
+import { ClienteCreate, ClienteIndex } from "./controllers/clienteController"
+import { destinoCreate, destinoIndex, excurasaoCreate, excursaoIndex, reservaCreate, reservaIndex } from "./controllers/viagemController"
 
 
 
 
 const router = Router()
 
-router.get('/cliente',)
-      .post('/cliente',)
+router.get('/cliente',ClienteIndex)
+      .post('/cliente',ClienteCreate)
+    
+    
+    
+router.get('/viagem/reserva',reservaIndex)
+      .post('/viagem/reserva',reservaCreate)
+      
+      .get('/viagem/excusao',excursaoIndex)
+      .post('/viagem/excursao',excurasaoCreate)
+
+      .get('/viagem/destino',destinoIndex)
+      .post('/viagem/destino',destinoCreate)
+
 
 export default router
