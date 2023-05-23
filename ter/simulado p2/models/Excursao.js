40f9643e615b99e3f614696fc5e7da7a7f99dba3
dode2511 <81193494+dode2511@users.noreply.github.com>
2023-05-23 09:29:48 -0300

    Update Excursao.js

---
+++ ter/simulado p2/models/Excursao.js
@@ -0,0 +1,42 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../data/conecta.js';
+import {Cliente} from "./Cliente.js" 
+
+export const Excursao = sequelize.define('excursao', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  local: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+  tipo: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  preco: {
+    type: DataTypes.DECIMAL(4,2),
+    allowNull: false,
+  },
+  data: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+
+
+Reserva.belongsTo(Cliente, {
+    foreignKey: {
+      name: 'Cliente_id',
+      allowNull: false
+    },    
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE'
+  })
+  
+    Cliente.hasMany(Reserva, {
+    foreignKey: 'Cliente_id',
+  })
+
