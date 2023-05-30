119df8ae366cd48c156a71592cce1b1a9016bbc1
dode2511 <81193494+dode2511@users.noreply.github.com>
2023-05-30 11:05:39 -0300

    a

---
+++ ter/Trabalho 2/controllers/cursoController.js
@@ -0,0 +1,34 @@
+import { Aluno } from "../models/Aluno.js";
+import { Curso } from "../models/Curso.js";
+import { Matricula } from "../models/Matricula.js";
+import { Professor } from "../models/Professor.js";
+
+
+export const cursoIndex = async (req, res) => {
+  try {
+    const cursos = await Curso.findAll(
+      { include: [Professor] }
+    );
+    res.status(200).json(cursos)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}
+
+export const cursoCreate = async (req, res) => {
+  const { nome,  descricao, endereco, turno,professor_id } = req.body
+
+  if (!nome || !descricao || !endereco || !turno || !professor_id ) {
+    res.status(400).json({ id: 0, msg: "Erro... Informe os dados" })
+    return
+  }
+
+  try {
+    const curso = await Curso.create({
+      nome, descricao, endereco, turno, professor_id
+    });
+    res.status(201).json(aluno)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}
--- ter/Trabalho 2/routes.js
+++ ter/Trabalho 2/routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express"
 import { AlunoCreate, alunoIndex } from "./controllers/alunoController.js"
+import { cursoCreate, cursoIndex } from "./controllers/cursoController.js"
 
 
 const router = Router()
 
 router.get('/alunos',alunoIndex )
       .post('/alunos', AlunoCreate)
+
+
+router.get('/cursos',cursoIndex )
+      .post('/cursos', cursoCreate)
 
 export default router
